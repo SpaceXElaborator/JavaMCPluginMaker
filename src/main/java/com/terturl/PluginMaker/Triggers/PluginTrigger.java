e4e18a94909b359408d312bd7a068fdd51b70d73
Sean Rahman <terturlminecraft@gmail.com>
2022-06-16 15:10:28 -0700

    Added a plugin trigger class to help with creating if statements later down the line

--- src/main/java/com/terturl/PluginMaker/Triggers/PluginTrigger.java
+++ src/main/java/com/terturl/PluginMaker/Triggers/PluginTrigger.java
@@ -10,8 +10,11 @@ import com.terturl.PluginMaker.Effects.PluginEffect;
 
 public abstract class PluginTrigger {
 
+	public boolean requiresPlayer = false;
+	
 	private String canonicalName;
 	private List<PluginEffect> effects = new ArrayList<>();
+	private List<PluginTrigger> triggers = new ArrayList<>();
 	
 	public abstract String getTemplateFile();
 	public abstract Map<String, Object> getTempMappings();
@@ -25,12 +28,29 @@ public abstract class PluginTrigger {
 		canonicalName = s;
 	}
 	
+	public void addPluginTrigger(PluginTrigger pt) {
+		pt.setCanonicalName(getCanonicalName() + "Trigger" + String.valueOf(triggers.size()));
+		triggers.add(pt);
+	}
+	
 	public void addPluginEffect(PluginEffect pe) {
-		pe.setCanonicalName("Effect" + String.valueOf(effects.size()));
+		pe.setCanonicalName(getCanonicalName() + "Effect" + String.valueOf(effects.size()));
 		effects.add(pe);
 	}
 	
+	public void removeListenerEffect(PluginEffect pe) {
+		effects.remove(pe);
+	}
+	
+	public void removePluginTrigger(PluginTrigger pt) {
+		triggers.remove(pt);
+	}
+	
 	public boolean requiresPlayer() {
+		if(requiresPlayer == true) {
+			return true;
+		}
+		
 		for(PluginEffect pe : getEffects()) {
 			if(pe.requiresPlayer) return true;
 		}
@@ -44,9 +64,11 @@ public abstract class PluginTrigger {
 	public Map<String, Object> getMappings() {
 		Map<String, Object> temp = new HashMap<>();
 		temp.put(getCanonicalName() + "Effects", effects);
+		temp.put(getCanonicalName() + "Triggers", triggers);
 		getTempMappings().forEach((k, v) -> {
 			temp.put(getCanonicalName() + "Trigger_" + k, v);
 		});
+		System.out.println(temp.toString());
 		return temp;
 	}
 
